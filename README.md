55c331a0ce9fd8eadbe4dc8c7fdaa1ce77d92f76
Anton <antonkochergin4@gmail.com>
2025-12-14 00:29:11 +0400

    Версия с работающим независимым exe-шником и кнопкой с вводном и выводом текста

---
+++ README.md
@@ -0,0 +1,3 @@
+cd /c/MyApp/build
+cmake .. -G "MinGW Makefiles"
+mingw32-make
--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,124 @@
-#define wxUSE_WARNING_FOR_COMMON_CONTROLS 0
+п»ї// Include the main wxWidgets header file
+// It contains everything needed for GUI: windows, buttons, events, etc.
 #include <wx/wx.h>
 
-class MyFrame : public wxFrame {
+// ---------------------------------------------------------------
+// 1. Declare the main window class (inherited from wxFrame)
+// ---------------------------------------------------------------
+class MyFrame : public wxFrame
+{
 public:
+    // Constructor: creates the window and all its elements
     MyFrame();
 
 private:
-    void OnButtonClick(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
+    // Event handlers вЂ” functions called when the user performs actions
+    void OnButtonClick(wxCommandEvent& event);      // when the button is clicked
+    void OnAbout(wxCommandEvent& event);            // when "About" is selected
+    void OnExit(wxCommandEvent& event);             // when exiting the app
 
+    // Pointers to UI elements (so they can be accessed from event handlers)
+    wxTextCtrl* m_textCtrl;   // text input field
+    wxButton* m_button;     // button
+
+    // Macro required for event handlers to be recognized
     wxDECLARE_EVENT_TABLE();
 };
 
-class MyApp : public wxApp {
+// ---------------------------------------------------------------
+// 2. Declare the application class (entry point)
+// ---------------------------------------------------------------
+class MyApp : public wxApp
+{
 public:
-    virtual bool OnInit();
+    virtual bool OnInit(); // called at application startup
 };
 
-enum {
-    ID_BUTTON = 1000,
-    ID_EXIT
-};
+// ---------------------------------------------------------------
+// 3. Implementation of the main window
+// ---------------------------------------------------------------
 
+// Event table: "when an event occurs в†’ call a function"
+// Without this, buttons and menu items won't work!
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-EVT_BUTTON(ID_BUTTON, MyFrame::OnButtonClick)
-EVT_MENU(ID_EXIT, MyFrame::OnExit)
+EVT_BUTTON(1001, MyFrame::OnButtonClick)  // button with ID=1001 в†’ OnButtonClick
+EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)    // "About" menu item в†’ OnAbout
+EVT_MENU(wxID_EXIT, MyFrame::OnExit)     // "Exit" menu item в†’ OnExit
 wxEND_EVENT_TABLE()
 
-bool MyApp::OnInit() {
+// Window constructor
+MyFrame::MyFrame()
+    : wxFrame(nullptr, wxID_ANY, "My wxWidgets Application", wxDefaultPosition, wxSize(400, 200))
+{
+    // --- Create the menu bar ---
+    wxMenu* menuFile = new wxMenu;
+    menuFile->Append(wxID_EXIT, "Exit\tAlt+F4");  // \t вЂ” keyboard shortcut
+
+    wxMenu* menuHelp = new wxMenu;
+    menuHelp->Append(wxID_ABOUT, "About..."); // standard ID
+
+    wxMenuBar* menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");     // & underlines the letter (Alt+F)
+    menuBar->Append(menuHelp, "&Help");
+    SetMenuBar(menuBar); // attach the menu bar to the window
+
+    // --- Create the status bar ---
+    CreateStatusBar();
+    SetStatusText("Ready");
+
+    // --- Create a text control ---
+    // ID = -1 в†’ auto-generated; we won't use it directly
+    m_textCtrl = new wxTextCtrl(this, wxID_ANY, "Enter text...", wxPoint(20, 30), wxSize(200, 30));
+
+    // --- Create a button ---
+    m_button = new wxButton(this, 1001, "Show Text", wxPoint(230, 30), wxSize(120, 30));
+    // ID = 1001 вЂ” used in the event table above
+}
+
+// Button click handler
+void MyFrame::OnButtonClick(wxCommandEvent& WXUNUSED(event))
+{
+    // Get text from the text field
+    wxString text = m_textCtrl->GetValue();
+
+    // Display the text in the status bar
+    SetStatusText("You entered: " + text);
+
+    // (optional) show a message box:
+    // wxMessageBox("You entered: " + text, "Info", wxOK | wxICON_INFORMATION);
+}
+
+// "About" menu handler
+void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
+{
+    wxMessageBox(
+        "This is a sample wxWidgets application.\n\n"
+        "Author: Anton\n"
+        "Date: 2025",
+        "About",
+        wxOK | wxICON_INFORMATION,
+        this  // specify parent window for modal dialog
+    );
+}
+
+// Exit handler
+void MyFrame::OnExit(wxCommandEvent& WXUNUSED(event))
+{
+    Close(true); // close the application
+}
+
+// ---------------------------------------------------------------
+// 4. Application entry point implementation
+// ---------------------------------------------------------------
+
+// Called once at startup
+bool MyApp::OnInit()
+{
     MyFrame* frame = new MyFrame();
-    frame->Show(true);
+    frame->Show(true); // show the window
     return true;
 }
 
-MyFrame::MyFrame()
-    : wxFrame(nullptr, wxID_ANY, "Моё первое wxWidgets-приложение", wxDefaultPosition, wxSize(400, 200))
-{
-    // Панель (обязательна в wxWidgets для размещения контролов)
-    wxPanel* panel = new wxPanel(this);
-
-    // Кнопка
-    wxButton* button = new wxButton(panel, ID_BUTTON, "Нажми !", wxPoint(140, 50));
-
-    // Меню
-    wxMenu* menuFile = new wxMenu;
-    menuFile->Append(ID_EXIT, "Выход\tAlt+F4");
-
-    wxMenuBar* menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&Файл");
-    SetMenuBar(menuBar);
-
-    // Статусная строка
-    CreateStatusBar();
-    SetStatusText("Готово");
-}
-
-void MyFrame::OnButtonClick(wxCommandEvent& WXUNUSED(event)) {
-    wxMessageBox("Привет Сделал !", "Сообщение", wxOK | wxICON_INFORMATION);
-}
-
-void MyFrame::OnExit(wxCommandEvent& event) {
-    Close(true);
-}
-
+// Macro that registers the application with wxWidgets
+// This is the equivalent of main() for wxWidgets apps
 wxIMPLEMENT_APP(MyApp);
